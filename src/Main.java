f77fad55a266c865717f367871930afb6327ffa6
Minh09470203 <10a10nguyenducminh@gmail.com>
2026-05-14 22:25:01 +0700

    Final update

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,16 @@ import com.formdev.flatlaf.FlatLightLaf; // Import FlatLaf
 
 public class Main {
     public static void main(String[] args) {
-        // 1. KÍCH HOẠT GIAO DIỆN FLATLAF (Phải làm trước khi vẽ UI)
         try {
             UIManager.setLookAndFeel(new FlatLightLaf()); 
-            // Nếu thích màu tối ngầu ngầu thì dùng: new com.formdev.flatlaf.FlatDarkLaf()
         } catch (Exception ex) {
             System.err.println("Không thể khởi tạo giao diện FlatLaf!");
         }
 
-        // 2. KHỞI CHẠY LUỒNG ĐĂNG NHẬP
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 LoginView view = new LoginView();
-                // Nạp View vào Controller để Controller quản lý
                 new LoginController(view);
                 view.setVisible(true);
             }
